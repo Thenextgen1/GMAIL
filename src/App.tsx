61557633b224c5babac6aab2a1707e854664efa6
Thenextgen1 <elijahohimairei@gmail.com>
2022-09-30 16:16:40 +0100

    created page route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,77 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Inbox,
+  AllMail,
+  Chats,
+  Drafts,
+  Important,
+  Lists,
+  Scheduled,
+  Sent,
+  Snoozed,
+  Spam,
+  Starred,
+  Trash,
+} from "./pages";
 import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Inbox />}
+          />
+          <Route
+            path="/allMail"
+            element={<AllMail />}
+          />
+          <Route
+            path="/chats"
+            element={<Chats />}
+          />
+          <Route
+            path="/drafts"
+            element={<Drafts />}
+          />
+          <Route
+            path="/important"
+            element={<Important />}
+          />
+          <Route
+            path="/lists"
+            element={<Lists />}
+          />
+          <Route
+            path="/scheduled"
+            element={<Scheduled />}
+          />
+          <Route
+            path="/sent"
+            element={<Sent />}
+          />
+          <Route
+            path="/spam"
+            element={<Spam />}
+          />
+          <Route
+            path="/snoozed"
+            element={<Snoozed />}
+          />
+          <Route
+            path="/starred"
+            element={<Starred />}
+          />
+          <Route
+            path="/trash"
+            element={<Trash />}
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
